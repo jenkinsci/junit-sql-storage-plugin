033cd98e538d9c9f18cc20e0dc3675bb725a6ca6
Tim Jacomb <timjacomb1+github@gmail.com>
2020-09-22 07:49:58 +0100

    Add schema management (flyway)

---
+++ src/main/java/io/jenkins/plugins/junit/storage/database/DatabaseSchemaLoader.java
@@ -0,0 +1,31 @@
+package io.jenkins.plugins.junit.storage.database;
+
+import hudson.init.Initializer;
+import io.jenkins.plugins.junit.storage.JunitTestResultStorageConfiguration;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.flywaydb.core.Flyway;
+
+import static hudson.init.InitMilestone.SYSTEM_CONFIG_ADAPTED;
+
+public class DatabaseSchemaLoader {
+    
+    public static boolean MIGRATED; 
+    
+    @Initializer(after = SYSTEM_CONFIG_ADAPTED)
+    public static void migrateSchema() throws SQLException {
+        JunitTestResultStorageConfiguration configuration = JunitTestResultStorageConfiguration.get();
+        if (configuration.getStorage() instanceof DatabaseTestResultStorage) {
+            DatabaseTestResultStorage storage = (DatabaseTestResultStorage) configuration.getStorage();
+            DataSource dataSource = storage.getConnectionSupplier().database().getDataSource();
+            Flyway flyway = Flyway
+                    .configure(DatabaseSchemaLoader.class.getClassLoader())
+                    .baselineOnMigrate(true)
+                    .table("junit_flyway_schema_history")
+                    .dataSource(dataSource)
+                    .load();
+            flyway.migrate();
+            MIGRATED = true;
+        }
+    }
+}
